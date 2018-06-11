9f294d4b304a8579d56c5c81c631dd03f8cf8c7a
liushuang <269620154@qq.com>
2018-06-11 22:35:58 +0800

    add test02

--- test01.sh
+++ test01.sh
@@ -2,6 +2,18 @@
 
 # a shell for test01
 
+value=90;
 echo hello ; echo world
-
-
+echo $value
+echo "$value"
+echo '$value'
+hello=$value
+echo $hello
+a=`echo hello!` # 把'echo'命令的结果传给变量'a'
+echo $a
+a=`ls -l`    #把'ls -l'的结果赋值给'a'
+echo $a
+#echo "$a"
+exit 0
+#+
+#+
---
+++ test02.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#int or string for shell
+
+a=234        #整型
+let "a += 1"
+echo "a = $a"
+echo        #还是整型
+
+
+b=${a/23/bb}     #把a里面的23替换成bb,将把ｂ从整型变成字符串
+echo  "b = $b"
+
+echo $0 ; echo $1; echo $2;
+
+echo "eee\$b"
+echo "eee$b"
+echo 'eee$b'
+
+b=$a;
+
+if cmp a a &>  /dev/null
+then echo "files a and b are identical"
+else echo "files a and b differ"
+fi
+
+x=3; y=3;
+#if [ $x -gt 2 ] 
+if (($x > 3))
+ then echo "x=y"
+elif [ $x -ne 4 ]
+ then echo "x!=y"
+fi
+
+
+echo $?
+exit $?
